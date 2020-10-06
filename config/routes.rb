3c09e7e8edbf72443acef114d17ac39632d73087
ryuichi-0913 <ryukishi25@gmail.com>
2020-10-06 09:29:47 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+}
+
+
+
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+}
+root 'home#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
