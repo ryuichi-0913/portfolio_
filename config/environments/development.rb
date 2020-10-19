a95405963083f1b1b50348e46042fcbeaf6fac4d
ryuichi-0913 <ryukishi25@gmail.com>
2020-10-19 09:32:10 +0000

    [delete]pas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
         address:              'smtp.gmail.com',
         domain:               'gmail.com',
         user_name:            'ryukishi25@gmail.com',
-        password:             'lhyisgjtjobbuixt',
+        password:             '',
         authentication:       'login',
         enable_starttls_auto: true
     }
